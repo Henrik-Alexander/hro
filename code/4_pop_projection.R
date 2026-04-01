fd9e417d555d91ccfd1a6afe1a72a15be6b40083
Schubert <schubert@demogr.mpg.de>
2026-04-01 13:41:56 +0200

    Upload the pop projection visualizations

---
+++ code/4_pop_projection.R
@@ -0,0 +1,140 @@
+######
+#Description
+library(readxl)
+library(tidyverse)
+library(dplyr)
+##################visualisation of the past 
+
+# Load the graphic template
+source("code/graphic_template.R")
+
+# Load the insgesamt data
+Rostock_past <- read_csv("data/insgesamt.csv")
+
+#wide format 
+Rostock_past_final <- Rostock_past %>%
+  group_by(year)%>%
+  summarise(Gesamtanzahl = sum(einwohnerzahl))%>%
+  mutate(Jahr = year)%>%
+  select(Jahr, Gesamtanzahl)
+
+
+ggplot(data=Rostock_past_final, mapping = aes(Jahr, Gesamtanzahl))+
+  geom_line(linetype="dashed",color= "2c3e50")+
+  ylim(190000,220000)+
+  theme_minimal()
+
+##########pop forecast 
+
+#Load data ---
+data <- read_excel(path = "projection/HRO_Ergebnisse_2023-2040_AG.xlsx")
+head(data)
+
+#Analysis / Visualisation ----
+
+#Merge the age groups 
+HRO_years <- data %>%
+  group_by(Jahr)%>%
+  summarise(Gesamtanzahl = sum(Anzahl,na.rm=T))%>%
+  filter(Jahr > 2024)
+
+
+#Plot the overall projection
+ggplot(data=HRO_years, mapping = aes(Jahr, Gesamtanzahl))+
+  geom_line(linetype="dashed",color= "2c3e50")+
+  ylim(190000,220000)+
+  theme_minimal()
+
+
+HRO_past_future <- bind_rows(Rostock_past_final, HRO_years)
+
+ggplot(data=HRO_past_future, mapping = aes(Jahr, Gesamtanzahl)) +
+  geom_vline(xintercept = 2024, linetype = "dashed", color = "grey") +
+  geom_line(data = filter(HRO_past_future, Jahr <= 2024), linetype = "solid", linewidth = 1) +
+  geom_point(data = filter(HRO_past_future, Jahr <= 2024), linewidth = 1) +
+  geom_line(data = filter(HRO_past_future, Jahr >= 2024), linetype = "dashed", linewidth = 1) +
+  scale_x_continuous(n.breaks = 10, expand = c(0, 0.2)) +
+  scale_y_continuous("Bevölkerungsgröße", labels = scales::label_number(scale = 1e-3, suffix = " K"), expand = c(0.3, 0), n.breaks = 8) 
+ggsave(filename = "Figures/projection_pop_size.pdf", height = 18, width = 22, unit = "cm")
+
+#Additional visualisations ----
+#Plot the projection for children vs working adults vs pensioniers 
+HRO_work <- data %>%
+  mutate(Lebensphase = case_when(
+    AG %in% c("unter 3","3 bis unter 6","6 bis unter 10","10 bis unter 15","15 bis unter 18")~ "Kinder und Jugendliche",
+    AG %in% c("18 bis unter 25","25 bis unter 45","45 bis unter 65")~ "Erwerbstätige",
+    AG %in% c("65 bis unter 80","80+")~"Rentner"))%>%
+  group_by(Lebensphase, Jahr)%>%
+  summarise(Summe = sum(Anzahl))
+
+
+# Plot the age groups
+ggplot(data=HRO_work, mapping = aes(Jahr, Summe,color=Lebensphase, group=Lebensphase, shape = Lebensphase))+
+  geom_line() +
+  geom_point() +
+  geom_text(data=subset(HRO_work, Jahr == 2030), aes(label = Lebensphase), vjust = -1) +
+  scale_x_continuous(n.breaks = 10, expand = c(0, 0.2)) +
+  scale_y_continuous("Bevölkerungsgröße", labels = scales::label_number(scale = 1e-3, suffix = " K"), expand = c(0.2, 0), n.breaks = 8) +
+  guides(color = "none", shape = "none")
+ggsave(filename = "Figures/projection_dependency_counts.pdf", height = 18, width = 22, unit = "cm")
+
+# Estimate the dependency ratios
+HRO_ratios <- HRO_work %>% 
+  pivot_wider(names_from = "Lebensphase", values_from = "Summe") %>% 
+  mutate(kind_ratio = Erwerbstätige / `Kinder und Jugendliche`,
+         rentner_ratio = Erwerbstätige / Rentner,
+         abhängiger_ratio = Erwerbstätige / (`Kinder und Jugendliche` + Rentner)) %>% 
+  pivot_longer(cols = ends_with("ratio"))
+
+# Create the ratio figure
+ggplot(data=HRO_ratios, aes(x=Jahr, y=value,color=name, group=name, shape = name))+
+  geom_hline(yintercept = 1) +
+  geom_line() +
+  geom_point() +
+  geom_text(data=subset(HRO_ratios, Jahr == 2030), aes(label = paste("pro", str_to_title(str_remove(name, "_ratio")))), vjust = -1) +
+  scale_x_continuous(n.breaks = 10, expand = c(0, 0.2)) +
+  scale_y_log10("Erwerbstätige",limits = c(1, 5), n.breaks = 10) +
+  guides(color = "none", shape = "none")
+ggsave(filename = "Figures/projection_dependency_ratio.pdf", height = 18, width = 22, unit = "cm")
+
+
+#Plot the projection for the different age groups using a barplot 
+data_diff_2023_2040 <- data %>%
+  pivot_wider(names_from = Jahr,values_from = "Anzahl")%>%
+  select("AG","2023","2040") %>%
+  mutate(Veränderung = `2040` - `2023`)
+
+
+# Sort the data
+data_diff_2023_2040 <- data_diff_2023_2040 %>%
+  mutate(AG = factor(AG, levels=c("unter 3", "3 bis unter 6", "6 bis unter 10", 
+                                  "10 bis unter 15", "15 bis unter 18", "18 bis unter 25",
+                                  "25 bis unter 45","45 bis unter 65","65 bis unter 80",
+                                  "80+")))
+
+# Absolute Veränderung
+ggplot(data_diff_2023_2040, aes(x=AG,y=Veränderung,fill=Veränderung>0))+
+  geom_col(color="white")+
+  geom_hline(yintercept = 0) +
+  geom_text(aes(label = round(Veränderung)), hjust = -0.2, family = "serif") +
+  # labs(title = "Absolute Veränderungen der Bevölkerungsanzahl von 2023 zu 2040 nach Altersgruppen")+
+  scale_x_discrete("Altersgruppe") + 
+  scale_y_continuous("Absolute Bevölkerungsveränderung") +
+  coord_flip()+
+  scale_fill_manual(values = c("TRUE"  = "#2ecc71","FALSE" = "#e74c3c"), guide = "none")
+ggsave(filename = "Figures/projection_pop_change_absolute.pdf", height = 18, width = 22, unit = "cm")
+
+
+ggplot(data_diff_2023_2040, aes(x=AG,y=Veränderung/`2023`,fill=Veränderung>0))+
+  geom_col(color="white")+
+  geom_hline(yintercept = 0) +
+  geom_text(aes(label = paste0(round(Veränderung/`2023` * 100, 1), "%"), y = 1.1 * Veränderung/`2023`),  family = "serif") +
+  # labs(title = "Absolute Veränderungen der Bevölkerungsanzahl von 2023 zu 2040 nach Altersgruppen")+
+  scale_x_discrete("Altersgruppe") + 
+  scale_y_continuous("Bevölkerungsveränderung (%)", labels = scales::percent, n.breaks = 10) +
+  coord_flip()+
+  scale_fill_manual(values = c("TRUE"  = "#2ecc71","FALSE" = "#e74c3c"), guide = "none")
+ggsave(filename = "Figures/projection_pop_change_relative.pdf", height = 18, width = 22, unit = "cm")
+# hjust = ifelse(Veränderung>0, 0.5, -0.5)
+
+### END #######################################################################
--- code/4_population_projection.R
+++ code/4_population_projection.R
@@ -7,6 +7,7 @@
 
 library(data.table)
 library(tidyverse)
+library(haven)
 
 ## Structure:
 # 1. Population structure in the jump off year
@@ -15,69 +16,20 @@ library(tidyverse)
 
 # Meta-parameter =====================================================
 
-# 
-jump_off_year <- 2024
-forecasting_horizon <- 30
-time_speps <- 5
-t <- seq(jump_off_year, jump_off_year+forecasting_horizon, by = time_speps)
+# Load the population projection
+pop_proj_hro <- read_xlsx("projection/HRO_Ergebnisse_2023-2040_AG.xlsx")
 
-# Age groups
-max_age <- 110
+# Make the data into data.table
+pop_proj_hro <- as.data.table(pop_proj_hro)
 
-# Population structure ===============================================
-
-# Population structure comes from Regionaldatenbank of the German Statistical Office
-
-# 
-header_pop <- read.csv("raw/pop_structure_gesis.csv", skip = 5, nrows = 1, sep = ";", header = F)
-header_pop <- header_pop[1, ]
-header_pop[is.na(header_pop)] <- "Estimate"
-pop_2024 <- read.csv("raw/pop_structure_gesis.csv", skip = 7, sep = ";", header = T, col.names = header_pop)
-names(pop_2024)[1:2] <- c("code", "municipality") 
-
-# Clean the column names
-names(pop_2024) <- str_to_lower(str_replace_all(names(pop_2024), "\\.", "_"))
-pop_2024 <- pop_2024[, !str_detect(names(pop_2024), "^estimate_")]
-names(pop_2024) <- paste0(names(pop_2024), c("", "", rep(c("_men", "_women"), each = 17)))
-
-# Reshape to long format
-pop_2024 <- pivot_longer(pop_2024,
-                         cols = ends_with("men"),
-                         names_to = "group",
-                         values_to = "pop")
+# Plot the aggregate
+pop_proj_hro[, .(N = sum(Anzahl)), by = Jahr] %>% 
+  ggplot(aes(x=Jahr, y = N)) + 
+  geom_line()
 
 
-# Create the sex column
-pop_2024 <- pop_2024 %>% 
-  mutate(sex = ifelse(str_detect(group, "_women"), "women", "men"),
-         age_lower = ifelse(str_detect(group, "^unter"), 0, str_extract(group, "\\d+")),
-          age_upper = ifelse(str_detect(group, "und_mehr"), max_age, str_remove(str_extract(group, "\\d+_jahre"), "_jahre")))
 
-
-# Make age numeric
-pop_2024 <- pop_2024 %>%
-  mutate(age_lower = as.numeric(age_lower),
-         age_upper = as.numeric(age_upper),
-         pop = as.numeric(pop))
-
-# Extract Rostock
-pop_2024_hro <- pop_2024 %>%
-  filter(municipality == "Rostock, kreisfreie Stadt")
-
-
-# Plot the population pyramide
-ggplot(data = pop_2024_hro, aes(x = age_lower, y = ifelse(sex == "men", -pop, pop), group = sex, colour=sex)) +
-  geom_step(linewidth = 1.5) + 
-  geom_hline(yintercept = 0) +
-  coord_flip() +
-  scale_y_continuous(labels = abs, n.breaks = 10) +
-  scale_x_continuous("Age", expand = c(0, 0))
-  
-# 2. Estimate the demographic processes =====================================
-
-# 
-hro_processes <- read.csv("raw/bewegung_natuerlich/bewegung_natuerlich_2019.csv")
-
-
+# Change in the age structure
+pop_proj_hro[Jahr %in% c(2024, 20240)]
 
 ### END ###############################################################
